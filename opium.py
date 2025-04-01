f50a05339c4235dbc7d71641ef1deb0a82737403
armen78782 <112015796+armen78782@users.noreply.github.com>
2025-04-01 19:15:25 +0400

    Update opium.py

--- opium.py
+++ opium.py
@@ -7,22 +7,20 @@ GITHUB_REPOS = [
 ]
 
 def search_github_files(query):
-    try:
-        response = requests.get(github_api_url)
-        if response.status_code == 200:
-            files = response.json()
-            results = [file['name'] for file in files if query.lower() in file['name'].lower()]
-            if results:
-                pystyle.Write.Print("\n[+] Найденные файлы:", pystyle.Colors.green, interval=0.0001)
-                for file in results:
-                    pystyle.Write.Print(f" - {file}", pystyle.Colors.green, interval=0.0001)
+    found = False
+    for url in GITHUB_REPOS:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.text
+                if query.lower() in data.lower():
+                    found = True
+                    pystyle.Write.Print(f"\n[+] Найдено в файле: {url}", pystyle.Colors.green, interval=0.0001)
             else:
-                pystyle.Write.Print("\n[-] Файлы не найдены", pystyle.Colors.red, interval=0.0001)
-        else:
-            pystyle.Write.Print(f"\n[!] Ошибка запроса: {response.status_code}", pystyle.Colors.red, interval=0.0001)
-    except Exception as e:
-        pystyle.Write.Print(f"\n[!] Ошибка: {e}", pystyle.Colors.red, interval=0.0001)
-
+                pystyle.Write.Print(f"\n[!] Ошибка запроса: {response.status_code}", pystyle.Colors.red, interval=0.0001)
+        except Exception as e:
+            pystyle.Write.Print(f"\n[!] Ошибка: {e}", pystyle.Colors.red, interval=0.0001)
+    
     if not found:
         pystyle.Write.Print("\n[!] Ничего не найдено\n", pystyle.Colors.blue_to_white, interval=0.0001)
 
@@ -45,12 +43,12 @@ def menu():
 
 if __name__ == "__main__":
     while True:
-    try:
-        pystyle.System.Clear()
-        query = pystyle.Write.Input("\n[+] Введите запрос для поиска в GitHub: ", pystyle.Colors.blue_to_white, interval=0.0001)
-        if query:
-            search_github_files(query)
-            pystyle.Write.Input("\n[+] Нажмите Enter для продолжения...", pystyle.Colors.blue_to_white, interval=0.0001)
-    except KeyboardInterrupt:
-        pystyle.Write.Print("\n[-] Выход...", pystyle.Colors.red, interval=0.0001)
-        exit()
+        try:
+            pystyle.System.Clear()
+            query = pystyle.Write.Input("\n[+] Введите запрос для поиска в GitHub: ", pystyle.Colors.blue_to_white, interval=0.0001)
+            if query:
+                search_github_files(query)
+                pystyle.Write.Input("\n[+] Нажмите Enter для продолжения...", pystyle.Colors.blue_to_white, interval=0.0001)
+        except KeyboardInterrupt:
+            pystyle.Write.Print("\n[-] Выход...", pystyle.Colors.red, interval=0.0001)
+            exit()
